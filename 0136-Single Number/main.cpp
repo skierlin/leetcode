48d7d4d821f38bc4b6118bf44347a4734407a2cc
skier_lin <663250456@qq.com>
2019-08-22 17:34:23 +0800

    add 235 and 236

---
+++ 0136-Single Number/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        assert(nums.size()%2 == 1);
+
+        unordered_set<int> hashtable;
+        for(int i = 0 ; i < nums.size() ; i ++)
+            if(hashtable.find(nums[i]) == hashtable.end())
+                hashtable.insert(nums[i]);
+            else
+                hashtable.erase(nums[i]);
+
+        assert(hashtable.size() == 1);
+        return *hashtable.begin();
+    }
+};
